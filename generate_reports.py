2b0045b409c55140eb9b66088bd31f58f72d8122
Jimmy Erickson <obx18.jimmy@gmail.com>
2023-02-07 12:51:02 -0600

    Completed most functionality

--- generate_reports.py
+++ generate_reports.py
@@ -2,39 +2,63 @@ import os
 import pandas as pd
 import dataframe_image as dfi
 import PySimpleGUI as sg
+import datetime
+import shutil
+from openpyxl import load_workbook
+import calendar
 
 def generate_report(month):
+
+    subsplashReport = pd.read_csv("Files/Drop Reports Here/"+month+" SS.csv")
+    subsplashReport = subsplashReport[['first_name', 'last_name', 'subfund', 'net_amount', 'frequency', 'email']]
+    subsplashReport.rename(columns={'first_name':'First Name', 'last_name':'Last Name', 'subfund':'Sub Fund', 'net_amount':'Net Amount', 'frequency':'Frequency', 'email':'Email'}, inplace=True)
+    outputReport = subsplashReport
+
+    try:
+        planningCenterReport = pd.read_csv("Files/Drop Reports Here/"+month+" PCO.csv")
+        planningCenterReport = planningCenterReport[['donor_first_name', 'donor_last_name', 'fund', 'net_amount']]
+        planningCenterReport.rename(columns={'donor_first_name':"First Name", 'donor_last_name':"Last Name", 'fund':'Sub Fund', 'net_amount':'Net Amount'}, inplace=True)
+        outputReport = pd.concat([planningCenterReport, subsplashReport])
+    except:
+        print("Planning Center Report Not Found")
+
     
-    subsplashReport = pd.read_csv(month + "/SS.csv")
-    planningCenterReport = pd.read_csv(month + "/PCO.csv")
-
-    subsplashReport = subsplashReport[['first_name', 'last_name', 'subfund', 'net_amount', 'frequency']]
-    subsplashReport.rename(columns={'first_name':'First Name', 'last_name':'Last Name', 'subfund':'Sub Fund', 'net_amount':'Net Amount', 'frequency':'Frequency'}, inplace=True)
-
-    planningCenterReport = planningCenterReport[['donor_first_name', 'donor_last_name', 'fund', 'net_amount']]
-    planningCenterReport.rename(columns={'donor_first_name':"First Name", 'donor_last_name':"Last Name", 'fund':'Sub Fund', 'net_amount':'Net Amount'}, inplace=True)
-
-
-    outputReport = pd.concat([planningCenterReport, subsplashReport])
     outputReport = outputReport.sort_values(by=['Sub Fund'])
+    outputReport['Date'] = month
 
     for subFund in outputReport['Sub Fund'].unique():
         individualReport = outputReport.loc[outputReport['Sub Fund'] == subFund]
-        dfi.export(individualReport.style.hide(axis='index'), month + "/Individual Reports/" + subFund + " " + month + ".png")
-
-        if(os.path.isfile('/Continuous Reports/'+subFund+' report')):
-            #open excel file and add sheet to it
+        #dfi.export(individualReport.style.hide(axis='index'), month + "/Individual Reports/" + subFund + " " + month + ".png")
+        
+        out_file = 'Files/Generated Reports/'+subFund+' Giving Report.xlsx'
+        print(out_file)
+        
+        if(os.path.isfile(out_file)):
+            with pd.ExcelWriter(out_file, mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+                try:
+                    individualReport.to_excel(writer, sheet_name=month, startrow=writer.sheets[month].max_row, index=False)
+                except:
+                    individualReport.to_excel(writer, sheet_name=month, index=False)
+                individualReport.to_excel(writer,sheet_name='Complete Giving List',index=False,header=False,startrow=writer.sheets[month].max_row)
+            #open excel file
+            #add to master giving log
+            #add sheet to it
         else:
-            print("idk")
+            shutil.copy("Dont Touch/template.xlsx", out_file)
+            with pd.ExcelWriter(out_file, mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+                #reader = pd.read_excel(out_file, sheet_name='Complete Giving List')
+                individualReport.to_excel(writer, sheet_name=month, index=False)
+                individualReport.to_excel(writer,sheet_name= 'Complete Giving List',index=False,header=True)
+            
             #create excel file and add the correct sheet
             
-
-    outputReport.to_csv(month + "/" + month + "Staff Support Report.csv", index=False)
+    outputReport.to_csv("Files/Generated Reports/" + month + " Staff Support Report.csv", index=False)
 
 files = os.listdir(os.getcwd())
-months = [f for f in files if os.path.isdir(os.getcwd()+'/'+f) and f != "environment"]
+months = list(calendar.month_name)[1:]
+month_year = [i+" "+str(datetime.date.today().year) for i in months]
 
-layout = [[sg.Text("Please Select A Month:")], [sg.Combo(months)],[sg.Button("Submit")]]
+layout = [[sg.Text("Please Select A Month:")], [sg.Combo(month_year)],[sg.Button("Submit")]]
 
 
 window = sg.Window("CityLight Report Generator", layout, margins=(300,300))
